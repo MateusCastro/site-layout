8b221e56dc4d2c8d602328e9e4934f83402c2e94
Mateus Castro <mateus.castro.domingos@gmail.com>
2020-01-11 19:09:26 -0300

    serviços, diferencias e personas finalizado

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    navText: [
+      "<img src='../images/esquerda.svg'>",
+      "<img src='../images/direita.svg'>"
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      700: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
+});
